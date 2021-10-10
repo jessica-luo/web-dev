60c08f2034d7b71e92c9d8bdf9824e06176db4f9
jessicaluo <luo.jes@northeastern.edu>
2021-10-10 05:09:59 -0400

    a5 JavaScript fa21

--- public/a5/twitter/explorescreen/ExploreScreen.js
+++ public/a5/twitter/explorescreen/ExploreScreen.js
@@ -1,5 +1,5 @@
-import NavigationSidebar from "../NavigationSidebar/index.js";
-import WhoToFollowList from "../WhoToFollowList/index.js";
+import NavigationSidebar from "../navigationsidebar/index.js";
+import WhoToFollowList from "../whotofollowlist/index.js";
 import ExploreComponent from "./ExploreComponent.js";
 
 (function ($) {
